53bf435c7834b0d8a2af1fae7b231f7de112bcd8
Tommy Ku <zoo1929@outlook.com>
2018-10-10 06:46:14 +0800

    ADD: Update

--- components/countdown-timer-list.js
+++ components/countdown-timer-list.js
@@ -84,16 +84,15 @@ class CountdownTimerList extends HTMLElement {
   }
 
   render() {
-    let children;
+    let newVnode;
     if (Object.keys(this.timers).length > 0) {
       children = Object.values(this.timers).map((timer) => h(
         'countdown-timer', { attrs: { name: timer.name, 'end-at': timer.endAt, uuid: timer.uuid } }
       ));
+      newVnode = h('div', { attrs: { slot: 'content' } }, children);
     } else {
-      children = 'No Timer! Add one!';
+      newVnode = h('div', { attrs: { slot: '' } });
     }
-
-    const newVnode = h('div', { attrs: { slot: 'content' } }, children);
 
     this.contentDiv = patch(this.contentDiv, newVnode);
   }
